72c628885aa2abcb11f964ef1d8d27e19afc4238
awais416 <awaisshahbaz34@gmail.com>
2017-03-05 12:40:33 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace database_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pname = new System.Windows.Forms.TextBox();
+            this.ppos = new System.Windows.Forms.TextBox();
+            this.pid = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pssn = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pname);
+            this.groupBox1.Controls.Add(this.pssn);
+            this.groupBox1.Controls.Add(this.ppos);
+            this.groupBox1.Controls.Add(this.pid);
+            this.groupBox1.Location = new System.Drawing.Point(323, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(527, 355);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "physician";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(73, 322);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(359, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "position";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "employ id";
+            // 
+            // pname
+            // 
+            this.pname.Location = new System.Drawing.Point(166, 118);
+            this.pname.Name = "pname";
+            this.pname.Size = new System.Drawing.Size(180, 20);
+            this.pname.TabIndex = 3;
+            this.pname.TextChanged += new System.EventHandler(this.name_TextChanged);
+            // 
+            // ppos
+            // 
+            this.ppos.Location = new System.Drawing.Point(166, 255);
+            this.ppos.Name = "ppos";
+            this.ppos.Size = new System.Drawing.Size(180, 20);
+            this.ppos.TabIndex = 1;
+            this.ppos.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // pid
+            // 
+            this.pid.Location = new System.Drawing.Point(166, 57);
+            this.pid.Name = "pid";
+            this.pid.Size = new System.Drawing.Size(180, 20);
+            this.pid.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(408, 419);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(327, 147);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(534, 589);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "load";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(486, 7);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(161, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "CLINIC MANAGMENT SYSTEM";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // pssn
+            // 
+            this.pssn.Location = new System.Drawing.Point(166, 185);
+            this.pssn.Name = "pssn";
+            this.pssn.Size = new System.Drawing.Size(180, 20);
+            this.pssn.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "paging no";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 813);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox pname;
+        private System.Windows.Forms.TextBox ppos;
+        private System.Windows.Forms.TextBox pid;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pssn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace database_project
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection();
+        public Form1()
+        {
+            InitializeComponent();
+            con.ConnectionString = "Data Source=AWAIS\\SQLEXPRESS;Initial Catalog=styker;User ID=sa;Password=Centrino1?";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        
+       
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pname.Text == "")
+                {
+                    MessageBox.Show("Please provide data");
+                    return;
+                }
+
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = con;
+                command.CommandText = "Insert into physician (EmpID,Name,position,SSN) values ('" + pid.Text + "','" + pname.Text + "','" + ppos.Text + "','" + pssn.Text + "')";
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data Saved");
+                pid.Text = "";
+                pname.Text = "";
+                ppos.Text = "";
+                pssn.Text = "";
+
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pid.Text == ""|| pname.Text==""||pssn.Text==""||ppos.Text=="")
+                {
+                    MessageBox.Show("Select a record to delete");
+                    return;
+                }
+                SqlCommand command = new SqlCommand();
+                command.Connection = con;
+                command.CommandText = "Delete from Physician Where EmpID='" + pid.Text + "'";
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data Deleted");
+                pid.Text = "";
+                pname.Text = "";
+                pssn.Text = "";
+                ppos.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            pid.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            pname.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            ppos.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            pssn.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        /*
+void bindgrid()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection("Data Source=AWAIS-PC;Initial Catalog=hospital;User ID=awais416;Password=asdfgh");
+                SqlDataAdapter sda = new SqlDataAdapter("select * from Physician", conn);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt.DefaultView;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+         */
+
+private void button4_Click(object sender, EventArgs e)
+{
+    string constring = "Data Source=AWAIS-PC;Initial Catalog=hospital;User ID=awais416;Password=asdfgh";
+    SqlConnection conn = new SqlConnection(constring);
+    SqlCommand command = new SqlCommad("Select* from Physician;",con);
+    try 
+
+    {
+        SqlDataAdapter sda= new SqlDataAdapter();
+        sda.SelectCommand= command;
+        DataTable dbdataset = new DataTable();
+        sda.Fill(dbdataset);
+        BindingSource bsource = new BindingSource();
+        bsource.DataSource = dbdataset;
+        dataGridView1.DataSource=bsource;
+        sda.Update(dbdataset);
+
+    }
+
+    catch (Exception ex)
+    {
+        MessageBox.Show(ex.Message);
+    }
+}
+
+private void button1_Click(object sender, EventArgs e)
+{
+    try
+    {
+        if (pid.Text == "")
+        {
+            MessageBox.Show("Select a record to modify");
+            return;
+        }
+        SqlCommand command = new SqlCommand("Data Source=AWAIS-PC;Initial Catalog=hospital;User ID=awais416;Password=asdfgh");
+        command.Connection = con;
+        command.CommandText = "Update Physician Set Name='" + pname.Text + "' , SSN='" + pssn.Text + "',position='" + ppos.Text + "'where EmpID='" + pid.Text + "'";
+        con.Open();
+        command.ExecuteNonQuery();
+        con.Close();
+        MessageBox.Show("Data Updated");
+        pname.Text = "";
+       pid.Text = "";
+       
+       pssn.Text = "";
+       ppos.Text = "";
+       
+    }
+    catch (Exception ex)
+    {
+        MessageBox.Show(ex.Message);
+    }
+}
+      
+private void label8_Click(object sender, EventArgs e)
+{
+
+}
+
+private void groupBox1_Enter(object sender, EventArgs e)
+{
+
+}
+
+       
+        
+        
+        
+        
+        
+        
+        
+        
+  
+       
+        }
+
+    
+    
+    
+    
+    }
+
+
